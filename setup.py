ffe162134098719e87f7b05cd458dbea6fba784e
John Preston <john@ews-network.net>
2021-08-08 10:07:54 +0100

    Using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,49 +1,8 @@
-#!/usr/bin/env python
+# #!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MPL-2.0
 
-"""The setup script."""
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
-
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
-requirements = []
-
-setup_requirements = [
-    "pytest-runner",
-]
-
-test_requirements = [
-    "pytest>=3",
-]
-
-setup(
-    author="John Preston",
-    author_email="john@compose-x.io",
-    python_requires=">=3.7",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    description="Specifications for ECS Compose-X",
-    install_requires=requirements,
-    long_description=readme,
-    include_package_data=True,
-    keywords="ecs_composex_specs",
-    name="ecs_composex_specs",
-    packages=find_packages(include=["ecs_composex_specs", "ecs_composex_specs.*"]),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url="https://github.com/johnpreston/ecs_composex_specs",
-    version="0.0.8",
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
